6dd8647b5b97c0e386337af6a7447e05ed159060
ned29 <ned29051995>
2017-08-17 06:02:09 +0300

    ADD: test

--- app/src/test/java/com/example/nedvy/fsm/ExampleUnitTest.java
+++ app/src/test/java/com/example/nedvy/fsm/ExampleUnitTest.java
@@ -1,17 +1,75 @@
 package com.example.nedvy.fsm;
 
-import org.junit.Test;
+import android.widget.Button;
+import android.widget.Toast;
 
-import static org.junit.Assert.*;
+import com.example.nedvy.fsm.view.MainActivity;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowToast;
+
+import butterknife.BindView;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class)
+
 public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+
+    @BindView(R.id.lock)
+    private Button lock;
+
+    @BindView(R.id.unlock)
+    private Button unLock;
+
+    @BindView(R.id.lockx2)
+    private Button lock_2;
+
+    @BindView(R.id.unlockx2)
+    private Button unLock_2;
+
+    private MainActivity activity;
+
+    @Before
+    public void setup() {
+        activity = Robolectric.buildActivity(MainActivity.class)
+                .create().get();
     }
+
+    @Test
+    public void checkActivityNotNull() throws Exception {
+        assertNotNull(activity);
+    }
+
+    @Test
+    public void buttonClick() throws Exception {
+        MainActivity activity = Robolectric.buildActivity(MainActivity.class)
+                .create().get();
+        lock.performClick();
+        lock_2.performClick();
+        unLock.performClick();
+        unLock_2.performClick();
+        assertNotNull(lock);
+        assertThat(ShadowToast.getTextOfLatestToast(), equalTo("AlarmDisarmed_AllLocked"));
+        assertNotNull(lock_2);
+        assertThat(ShadowToast.getTextOfLatestToast(), equalTo("AlarmArmed_AllLocked"));
+        assertNotNull(unLock);
+        assertThat(ShadowToast.getTextOfLatestToast(), equalTo("AlarmDisarmed_DriverUnLocked"));
+        assertNotNull(unLock_2);
+        assertThat(ShadowToast.getTextOfLatestToast(), equalTo("AlarmDisarmed_DriverUnLocked"));
+    }
+
 }
